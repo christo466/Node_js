2691383eac95cf15ec058762a3e7b321d86ee831
christo johnson <113782762+christo466@users.noreply.github.com>
2024-06-26 10:30:47 +0530

    Delete second_largest.js

--- second_largest.js
+++
@@ -1,5 +0,0 @@
-let mylist = [1,4,6,2,7,9,4,12,5,6]
-let uniqueArray = [...new Set(mylist)]
-uniqueArray.sort((a, b) => a - b);
-let lengthOlist = uniqueArray.length
-console.log('the second largest number is: ',uniqueArray[lengthOlist-2])
